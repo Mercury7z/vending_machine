41c696e7073d415c902e112c60c3535b277fb3ad
KimBogdan <bogger.kim@gmail.com>
2025-09-19 16:17:54 +0600

    # 5 exceptions
    add exception on userInput wrong inputs

--- src/model/CardAcceptor.java
+++ src/model/CardAcceptor.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -35,12 +36,28 @@ public class CardAcceptor implements Payment{
         java.util.Random r = new Random();
         System.out.println("Можно пополнить баланс на 500 картой");
         System.out.println("Введите номер карточки");
-        int userCard = sc.nextInt();
+        int userCard = 0;
+        try {
+            userCard = sc.nextInt();
+        }
+        catch (InputMismatchException e) {
+            sc.next();
+            System.out.println("не вводите буквы");
+            addBalance();
+        }
         System.out.println("Вам на карту номер "  + userCard + " отправлен 4 значный пин");
         int tempPin = r.nextInt(1000,10000);
         sendPinToUser(tempPin);
         System.out.println("Введите пин");
-        int userInputPin = sc.nextInt();
+        int userInputPin = 0;
+        try {
+            userInputPin = sc.nextInt();
+        }
+        catch (InputMismatchException e) {
+            sc.next();
+            System.out.println("не вводите буквы");
+            addBalance();
+        }
         if(userInputPin == tempPin) {
             if(checkUserBalance()) {
                 System.out.println("Платеж успешно прошел");
